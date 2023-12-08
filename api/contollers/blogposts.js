8ae4682d13b3b9bb942f55453965b2519b59875f
ShouvikD13 <deyshouvik499@gmail.com>
2023-12-08 16:31:11 +0530

    pre code merge

--- api/contollers/blogposts.js
+++ api/contollers/blogposts.js
@@ -1,3 +1,18 @@
+export const getPosts = (req,res)=>{
+    const q = req.query.cat ? "SELECT * FROM blogposts WHERE cat=?" : "SELECT * FROM blogposts";
+}
+
+export const getPost = (req,res)=>{
+    res.json("from controller")
+}
+
 export const addPost = (req,res)=>{
+    res.json("from controller")
+}
+
+export const deletePost = (req,res)=>{
+    res.json("from controller")
+}
+export const updatePost = (req,res)=>{
     res.json("from controller")
 }
