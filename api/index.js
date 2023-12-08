836d5c0b9eae53c3882ab4ccb97d1e15e3d5891c
ShouvikD13 <deyshouvik499@gmail.com>
2023-12-08 17:29:40 +0530

    test

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from "express"
-import postRoutes from "./routes/blogposts.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 
 const app = express()
 
 app.use(express.json())
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 
 app.listen(3000,()=>{
--- api/routes/blogposts.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { addPost } from "../contollers/blogposts.js"
-
-const router = express.Router()
-
-router.get("/", addPost)
-
-export default router
